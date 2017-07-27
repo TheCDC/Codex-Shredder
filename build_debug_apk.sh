c3c0bd45ead810c1429f765e422e3aeabc241731
Christopher Chen <christopher.chen1995@gmail.com>
2017-07-26 18:04:08 -0700

    Create apk building script

---
+++ build_debug_apk.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+react-native bundle --dev false --platform android --entry-file index.android.js --bundle-output ./android/app/build/intermediates/assets/debug/index.android.bundle --assets-dest ./android/app/build/intermediates/res/merged/debug
+
+cd android
+./gradlew assembleDebug
